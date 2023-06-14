b7b939e512b41166e206b1c671f221adab6b5fc4
Saullo Reis <saulloreis01@hotmail.com>
2023-06-14 10:23:23 -0300

    feat: placeholder

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
         <section className='header'>
             <h1 className='header-logo'>Recipes</h1>
             <form className='header-form'>
-                <input className='text' type={'text'}></input>
+                <input className='text' placeholder="Write the name of the food" type={'text'}></input>
                 <input className='button' type={'submit'} value={'Search'}></input>
             </form>
         </section>
