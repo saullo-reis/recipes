30d1a1a96ab033e1535b33307e7c1e2f6f1888ec
Saullo Reis <saulloreis01@hotmail.com>
2023-06-19 10:43:49 -0300

    feat: criando arquivo para armazenar as interfaces

---
+++ src/utils/utils.tsx
@@ -0,0 +1,17 @@
+export interface FoodProps {
+    strMeal: string,
+    strMealThumb: string,
+    idMeal: string
+}
+
+export interface AreasProps {
+    strArea: string,
+    map: void
+}
+
+export interface categoriesProps {
+    idCategory: string,
+    strCategory: string,
+    strCategoryThumb: string
+    map: void
+}
