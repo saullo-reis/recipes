d34da80609b64c61524aa545d1680800767ee968
Saullo Reis <saulloreis01@hotmail.com>
2023-06-19 17:29:04 -0300

    feat: adicionando sugestões de nomes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,58 @@
+"use client"
+
 import './HeaderStyle.sass'
 import Image from 'next/image'
 import Logo from '../../public/favicon.png' 
 import Link from 'next/link'
+import { FormEvent, FormEventHandler, useState } from 'react'
+import axios from 'axios'
+import { FoodProps } from '@/utils/utils'
 
 export const Header = () => {
+    const [searchNameFood, setSearchNameFood] = useState("")
+    const [errorStatus, setErrorStatus] = useState(false)
+    const [sugestion, setSugestions] = useState<FoodProps[]>([])
+
+    const search = async (e: string) => {
+        setSearchNameFood(e)
+        try{
+            const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${e}`)
+            setSugestions(response.data.meals)
+        }catch(err){
+            console.error(err)
+        }
+    }
+
+
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+        const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchNameFood}`)
+    }
+
     return (
         <section className='header'>
             <Link href={'/'}><Image className='header-logo' src={Logo} alt="Recipes logo" /></Link>
-            <form className='header-form'>
-                <input className='text' placeholder="Write the name of the food" type={'text'}></input>
-                <input className='button' type={'submit'} value={'Search'}></input>
-            </form>
+            <form className='header-form' onSubmit={(e) => handleSubmit(e)}>
+                <div>
+                    <input className='text' value={searchNameFood} onChange={(e) => search(e.target.value)} placeholder="Write the name of the food" type={'text'}></input>
+                    <input className='button' type={'submit'} value={'Search'}></input> 
+                </div>
+                {
+                    searchNameFood !== "" && <ul className='sugestions'>
+                        {
+                            sugestion?.map((element, index) => {
+                                return (
+                                    <li className='sugestions-itens' key={index} onClick={() => setSearchNameFood(element.strMeal)}>
+                                        <p className='sugestions-itens-name'>{element.strMeal}</p>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                }
+                
+                {errorStatus && <p className="error">Food name not found</p>}        
+            </form>     
         </section>
     )
 }
