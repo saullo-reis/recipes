add072422f4d6834b97be73a003db4653904565a
Saullo Reis <saulloreis01@hotmail.com>
2023-06-20 10:58:20 -0300

    refactor: retirando consolelog

--- src/app/areas/[slug]/page.tsx
+++ src/app/areas/[slug]/page.tsx
@@ -3,7 +3,6 @@ import { Footer } from "@/components/footer/Footer";
 import { Header } from "@/components/header/Header";
 
 export default function Page({params}: {params: {slug: string}}){
-    console.log(params)
     return (
         <>
             <Header />
